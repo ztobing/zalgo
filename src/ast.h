9e523025917147951fe05b52e4d76fa11316aa99
Zef <titotobing0511@gmail.com>
2019-10-27 23:17:56 +0700

    Add deconstructor code

--- src/ast.h
+++ src/ast.h
@@ -15,21 +15,31 @@ struct AST
     string value;
     AST(int, string);
     AST(const AST&);
+    ~AST();
 };
 
 AST::AST(int type, string value)
 {
     this->type = type;
     this->value = value;
-    this->left = this->right = NULL;
+    this->left = this->right = nullptr;
 }
 
 AST::AST(const AST& a)
 {
     this->type = a.type;
     this->value = a.value;
-    this->left = a.left != NULL ? new AST(*a.left) : NULL;
-    this->right = a.right != NULL ? new AST(*a.right) : NULL;
+    this->left = a.left != nullptr ? new AST(*a.left) : nullptr;
+    this->right = a.right != nullptr ? new AST(*a.right) : nullptr;
+}
+
+AST::~AST()
+{
+    // cout << "Deconstructor called for node " << value << endl;
+    // cout << "Left: " << left << endl;
+    // cout << "Right: " << right << endl;
+    // if (left != nullptr) delete left;
+    // if (right != nullptr) delete right;
 }
 
 #endif
